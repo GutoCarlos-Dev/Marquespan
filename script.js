b40cf4b266bf85e270586cdc7888c2e7faa23a5c
GULTEMBERGUE CARLOS REGO <gutoapollo@hotmail.com>
2025-08-04 11:05:21 -0300

    1

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const supabase = window.supabase.createClient(
-  'https://hlzcycvlcuhgnnjkmslt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsemN5Y3ZsY3VoZ25uamttc2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwODA1ODgsImV4cCI6MjA2OTY1NjU4OH0.GEm-OCzpScQ5uFvhkNFHxdKdwZc3W2bnxphq0pjBwxY'
-);
+const supabase = window.supabase;
 
 // CADASTRAR USUÁRIO
 if (document.getElementById('formUsuario')) {
