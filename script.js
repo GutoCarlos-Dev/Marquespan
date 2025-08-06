f266cae23378a0eed3e9e0495eb26d48f12250f7
GULTEMBERGUE CARLOS REGO <gutoapollo@hotmail.com>
2025-08-06 10:05:28 -0300

    1

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(
   'https://hlzcycvlcuhgnnjkmslt.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsemN5Y3ZsY3VoZ25uamttc2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwODA1ODgsImV4cCI6MjA2OTY1NjU4OH0.GEm-OCzpScQ5uFvhkNFHxdKdwZc3W2bnxphq0pjBwxY' // substitua pela sua chave pública real
