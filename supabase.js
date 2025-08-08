d941d4f872549be8e7ed8ab727f74739d9c25409
GULTEMBERGUE CARLOS REGO <gutoapollo@hotmail.com>
2025-08-08 15:38:01 -0300

    ok

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // script/supabase.js
 export const supabase = supabase.createClient(
   'https://hlzcycvlcuhgnnjkmslt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsemN5Y3ZsY3VoZ25uamttc2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwODA1ODgsImV4cCI6MjA2OTY1NjU4OH0.GEm-OCzpScQ5uFvhkNFHxdKdwZc3W2bnxphq0pjBwxY'
 );
