2ec2a90452ea139607f48f96d10ea769982f8f80
GULTEMBERGUE CARLOS REGO <gutoapollo@hotmail.com>
2025-08-14 09:07:25 -0300

    3

---
+++ script/cadastro-veiculos.js
@@ -0,0 +1,7 @@
+function abrirModal() {
+  document.getElementById("modalVeiculo").style.display = "flex";
+}
+
+function fecharModal() {
+  document.getElementById("modalVeiculo").style.display = "none";
+}
