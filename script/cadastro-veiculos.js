7969335f538ac6eb5ca71d7f549265599565a9ff
GULTEMBERGUE CARLOS REGO <gutoapollo@hotmail.com>
2025-08-19 15:16:08 -0300

    3

--- script/cadastro-veiculos.js
+++ script/cadastro-veiculos.js
@@ -1,4 +1,4 @@
-import { supabase } from '/script/supabase.js';
+import { supabase } from './supabase.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("formVeiculo");
