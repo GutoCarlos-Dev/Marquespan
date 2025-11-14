a138aaafade8735e72a22d5a7987604da4694a60
GutoCarlos-Dev <gutoapollo@hotmail.com>
2025-11-14 09:49:26 -0300

    2

--- script/pneu-updated.js
+++ script/pneu-updated.js
@@ -458,7 +458,7 @@ function initializeSelectsContagem() {
   // Predefined options
   const marcas = ['BRIDGESTONE', 'CONTINENTAL', 'GOODYEAR', 'MICHELIN', 'PIRELLI'];
   const modelos = ['225/75/16', '235/75/17.5', '275/80/22.5 - LISO', '275/80/22.5 - BORRACHUDO', '295/80/22.5 - LISO', '295/80/22.5 - BORRACHUDO'];
-  const tipos = ['NOVO', 'RECAPADO'];
+  const tipos = ['NOVO', 'RECAPADO', 'USADO'];
 
   selectMarca.innerHTML = '<option value="">Selecione</option>' + marcas.map(m => `<option value="${m}">${m}</option>`).join('');
   selectModelo.innerHTML = '<option value="">Selecione</option>' + modelos.map(m => `<option value="${m}">${m}</option>`).join('');
