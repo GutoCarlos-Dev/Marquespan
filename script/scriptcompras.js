527047d0f16aa76906b114d88536a6a6a0d0555e
GutoCarlos-Dev <gutoapollo@hotmail.com>
2025-12-08 17:27:33 -0300

    3

--- script/scriptcompras.js
+++ script/scriptcompras.js
@@ -849,8 +849,18 @@ const UI = {
 
   async renderProdutosGrid(){
     try {
-      const produtos = await SupabaseService.list('produtos', '*', {orderBy: this._produtosSort.field, ascending: this._produtosSort.ascending});
-      this.produtosTableBody.innerHTML = produtos.map(p => `<tr><td>${p.nome}</td><td>${p.codigo_principal}</td><td><button class="btn-delete" data-id="${p.id}">Excluir</button></td></tr>`).join('');
+      const produtos = await SupabaseService.list('produtos', 'id, codigo_principal, codigo_secundario, nome, unidade_medida', {orderBy: this._produtosSort.field, ascending: this._produtosSort.ascending});
+      this.produtosTableBody.innerHTML = produtos.map(p => `
+        <tr>
+          <td>${p.codigo_principal || ''}</td>
+          <td>${p.codigo_secundario || ''}</td>
+          <td>${p.nome || ''}</td>
+          <td>${p.unidade_medida || ''}</td>
+          <td>
+            <button class="btn-edit" data-id="${p.id}">Editar</button>
+            <button class="btn-delete" data-id="${p.id}">Excluir</button>
+          </td>
+        </tr>`).join('');
     } catch(e) {
       console.error('Erro ao carregar produtos', e);
     }
