fb924ca72667f86dac8b8c1d8f03601e528ff3e0
GutoCarlos-Dev <gutoapollo@hotmail.com>
2025-12-08 17:18:13 -0300

    3

--- script/supabase.js
+++ script/supabase.js
@@ -1,4 +1,5 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+// Correção: A URL do CDN foi trocada para uma compatível com módulos ES (esm.sh)
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const supabaseUrl = 'https://hlzcycvlcuhgnnjkmslt.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsemN5Y3ZsY3VoZ25uamttc2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwODA1ODgsImV4cCI6MjA2OTY1NjU4OH0.GEm-OCzpScQ5uFvhkNFHxdKdwZc3W2bnxphq0pjBwxY';
