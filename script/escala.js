10b484c5d5a39a13008940169e6a6d7c1febaa78
GutoCarlos-Dev <gutoapollo@hotmail.com>
2026-02-04 15:24:23 -0300

    1

--- script/escala.js
+++ script/escala.js
@@ -1114,11 +1114,11 @@ document.addEventListener('DOMContentLoaded', () => {
         
         // Campo Nome
         doc.line(margin, currentY, margin + 60, currentY); 
-        doc.text('Nome Completo:', margin, currentY + 6);
+        doc.text('Nome Completo:', margin, currentY + 5);
         
         // Campo Assinatura
         doc.line(margin + 65, currentY, margin + contentWidth, currentY);
-        doc.text('Assinatura:', margin + 65, currentY + 6);
+        doc.text('Assinatura:', margin + 65, currentY + 5);
 
         doc.save(`Boleta_${valor.replace(/[^a-z0-9]/gi, '_')}_${semana}.pdf`);
     }
